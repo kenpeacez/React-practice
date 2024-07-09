9fd9b0ab13b213ce011f3e1cefe5be51b256d459
kenpeacez <kenpeacezx@gmail.com>
2024-07-09 20:45:36 +0800

    First commit
    Udemy Tutorial
    Section 2 React Refresher

---
+++ vite-project/src/components/PostsList.jsx
@@ -0,0 +1,38 @@
+import classes from "./PostsList.module.css";
+import NewPost from "./NewPost";
+import Modal from "./Modal";
+import { useState } from "react";
+import Post from "./Post";
+
+function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPost] = useState([]);
+
+  function addPostHandler(postData) {
+    setPost((existingPosts) => [postData, ...existingPosts]);
+  }
+
+  return (
+    <>
+      {isPosting ? (
+        <Modal>
+          <NewPost onCancel={onStopPosting} onAddPosting={addPostHandler} />
+        </Modal>
+      ) : null}
+      {posts.length > 0 ? (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      ) : null}
+      {posts.length === 0 ? (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some posts.</p>
+        </div>
+      ) : null}
+    </>
+  );
+}
+
+export default PostsList;
