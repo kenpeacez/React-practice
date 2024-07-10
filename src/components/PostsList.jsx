9fc68940b671e932d17798fa59c1715319b2ec3d
kenpeacez <kenpeacezx@gmail.com>
2024-07-10 10:54:02 +0800

    No.38 - Handling Loading State

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,14 +6,19 @@ import Post from "./Post";
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPost] = useState([]);
+  //loading handler
+  const [isLoading, setIsLoading] = useState(false);
 
   //useEffect(() => {}, []) , Accepts 2 arguments, a function and an array, remember to import useEffect from react
+  //useEffect(<function>, <dependency>)
+
   async function fetchPosts() {
+    setIsLoading(true);
     const response = await fetch("http://localhost:8080/posts");
     const resData = await response.json();
     setPost(resData.posts);
+    setIsLoading(false);
   }
-
 
   useEffect(() => {
     fetchPosts();
@@ -21,6 +26,7 @@ function PostsList({ isPosting, onStopPosting }) {
 
   function addPostHandler(postData) {
     //backend API
+
     fetch("http://localhost:8080/posts", {
       method: "POST",
       headers: {
@@ -33,24 +39,29 @@ function PostsList({ isPosting, onStopPosting }) {
 
   return (
     <>
-      {isPosting ? (
+      {isPosting && (
         <Modal>
           <NewPost onCancel={onStopPosting} onAddPosting={addPostHandler} />
         </Modal>
-      ) : null}
-      {posts.length > 0 ? (
+      )}
+      {!isLoading && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
-      ) : null}
-      {posts.length === 0 ? (
+      )}
+      {!isLoading && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some posts.</p>
         </div>
-      ) : null}
+      )}
+      {isLoading && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
+        </div>
+      )}
     </>
   );
 }
